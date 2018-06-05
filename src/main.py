6b58b7f944bb0dc929590304d0389320bebe1c16
Tom Borchert <borcht@amazon.com>
2018-06-05 22:49:07 +0100

    fixed usage of sigma

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     if not param_values:
         param_values = Parameters({
-            # CMRO2 source: http://www.frca.co.uk/article.aspx?articleid=100344
+            # 2.5 - 4.5
             "CMRO2": [3.0] * units.mlO2 / units.hundred_g / units.min,
 
             # Length of the capillary.
@@ -26,29 +26,35 @@ if __name__ == '__main__':
 
             # The experimentally obtained average values of blood flow velocities in cerebral capillaries indicate
             # that these velocities vary mainly from 0.5 to 1.5 mm/sec.
+            # 0.5 - 1.5
             "velocity": [1.0] * units.mm / units.sec,
 
             # Diffusion coefficient (diffusivity) of oxygen in tissue.
             # http://rsif.royalsocietypublishing.org/content/royinterface/12/107/20150245.full.pdf
             # says this is 1.65e-5 cm^2/sec
+            # 5e-6 - 5e-5
             "D": [1.65e-5] * units.cm ** 2 / units.sec,
 
             # Radius of tissue cylinder.
+            # 20 - 40
             "r_Krogh": [20] * units.um,
 
             # Radius of capillary.
             "r_capillary": [3] * units.um,
 
             # Initial pressure at the top of the cylinder.
+            # 150 - 250 (later on)
             "paO2": [200] * units.mmHg,
 
             # Haemoglobin concentration.
+            # 10 - 12 (later on)
             "Hb": [10] * units.g / units.dL,
 
             "r_steps": [100],
             "z_steps": [1000],
 
-            "sigma": [3.1e-5] * units.mlO2 / units.hundred_ml / units.mmHg,
+            # sigma source: http://www.frca.co.uk/article.aspx?articleid=100344
+            "sigma": [0.0031] * units.mlO2 / units.dL / units.mmHg,
 
             "test": [True],
 
